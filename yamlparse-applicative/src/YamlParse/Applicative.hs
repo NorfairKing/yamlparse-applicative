2912b1bc84cd4f9893af8157bef5749a5745617f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-06 15:25:31 +0200

    some nice typeclass

--- yamlparse-applicative/src/YamlParse/Applicative.hs
+++ yamlparse-applicative/src/YamlParse/Applicative.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -10,8 +11,11 @@ module YamlParse.Applicative where
 -- import qualified Data.HashMap.Strict as HM
 
 import Control.Monad
-import Data.Text as T
-import Data.Text.IO as T
+import Data.Maybe
+import Data.Scientific
+import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Data.Text.IO as T
 import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Render.Text
 import Data.Vector (Vector)
@@ -19,8 +23,24 @@ import qualified Data.Vector as V
 import qualified Data.Yaml as Yaml
 
 someFunc :: IO ()
-someFunc =
-  forM_ (explainParser (fromYamlSchema :: Parser MyThing)) $ T.putStrLn . prettySchema
+someFunc = do
+  putStrLn "MyThing"
+  forM_ (explainParser (fromYamlSchema :: Parser MyThing)) $
+    T.putStrLn
+      . prettySchema
+  putStrLn ""
+  putStrLn "MyConfig"
+  forM_
+    (explainParser (fromYamlSchema :: Parser MyConfig))
+    $ T.putStrLn . prettySchema
+  putStrLn ""
+
+data MyConfig
+  = MyConfig {myConfigText :: Text, myConfigInt :: Maybe Int, myConfigList :: [Bool]}
+  deriving (Show, Eq)
+
+instance FromYamlSchema MyConfig where
+  fromYamlSchema = MyConfig <$> requiredField "myText" <*> optionalField "myInt" <*> optionalFieldWithDefault "list" []
 
 data MyThing = MyThing [Bool] Text
 
@@ -30,10 +50,32 @@ instance FromYamlSchema MyThing where
 class FromYamlSchema a where
   fromYamlSchema :: Parser a
 
+instance FromYamlSchema Bool where
+  fromYamlSchema = ParseBool
+
+instance FromYamlSchema Text where
+  fromYamlSchema = ParseString
+
+instance FromYamlSchema Int where
+  fromYamlSchema = (fromMaybe (error "floating or will cause over/under flow") . Data.Scientific.toBoundedInteger) <$> ParseNumber
+
+instance FromYamlSchema a => FromYamlSchema [a] where
+  fromYamlSchema = V.toList <$> ParseArray fromYamlSchema
+
+requiredField :: FromYamlSchema a => Text -> Parser a
+requiredField k = ParseField k $ FieldParserRequired fromYamlSchema
+
+optionalField :: FromYamlSchema a => Text -> Parser (Maybe a)
+optionalField k = ParseField k $ FieldParserOptional fromYamlSchema
+
+optionalFieldWithDefault :: (Show a, FromYamlSchema a) => Text -> a -> Parser a
+optionalFieldWithDefault k d = ParseField k $ FieldParserOptionalWithDefault fromYamlSchema d
+
 data Parser a where
   ParseAny :: Parser Yaml.Value
   ParseBool :: Parser Bool
   ParseString :: Parser Text
+  ParseNumber :: Parser Scientific
   ParseArray :: Parser a -> Parser (Vector a)
   ParseField :: Text -> FieldParser a -> Parser a
   -- ParseObject :: HashMap Text (FieldParser a) -> Parser (HashMap Text a)
@@ -52,6 +94,7 @@ instance Applicative Parser where
 data FieldParser a where
   FieldParserRequired :: Parser a -> FieldParser a
   FieldParserOptional :: Parser a -> FieldParser (Maybe a)
+  FieldParserOptionalWithDefault :: Show a => Parser a -> a -> FieldParser a
 
 (<?>) :: Parser a -> Text -> Parser a
 (<?>) = flip ParseComment
@@ -68,6 +111,7 @@ implementParser = go
       ParseAny -> pure
       ParseBool -> Yaml.withBool "Bool" pure
       ParseString -> Yaml.withText "Text" pure
+      ParseNumber -> Yaml.withScientific "Number" pure
       ParseArray p -> Yaml.withArray "Array" $ V.mapM (go p)
       ParseField k fp -> Yaml.withObject "Object" $ goField k fp
       ParsePure v -> const $ pure v
@@ -82,6 +126,11 @@ implementParser = go
         case mv of
           Nothing -> pure Nothing
           Just v -> Just <$> go p v
+      FieldParserOptionalWithDefault p d -> do
+        mv <- o Yaml..:? k
+        case mv of
+          Nothing -> pure d
+          Just v -> go p v
 
 -- FieldParserOptional p -> o Yaml..:? k >>= go p
 
@@ -95,22 +144,26 @@ explainParser = go
     go = \case
       ParseAny -> Just AnySchema
       ParseBool -> Just BoolSchema
+      ParseNumber -> Just NumberSchema
       ParseString -> Just StringSchema
       ParseArray p -> ArraySchema <$> go p
       ParseField k fp -> case fp of
-        FieldParserRequired p -> FieldSchema k True <$> go p
-        FieldParserOptional p -> FieldSchema k False <$> go p
+        FieldParserRequired p -> FieldSchema k True Nothing <$> go p
+        FieldParserOptional p -> FieldSchema k False Nothing <$> go p
+        FieldParserOptionalWithDefault p d -> FieldSchema k False (Just $ T.pack $ show d) <$> go p
       ParsePure _ -> Nothing
       ParseFmap _ p -> go p
       ParseAp pf p -> ApSchema <$> go pf <*> go p
       ParseComment t p -> CommentSchema t <$> go p
 
+-- TODO make schema a gadt?
 data Schema
   = AnySchema
   | BoolSchema
+  | NumberSchema
   | StringSchema
   | ArraySchema Schema
-  | FieldSchema Text Bool Schema
+  | FieldSchema Text Bool (Maybe Text) Schema
   | ApSchema Schema Schema -- We'll take this to mean 'and'
   | CommentSchema Text Schema
   deriving (Show, Eq)
@@ -125,8 +178,20 @@ schemaDoc = go
     go = \case
       AnySchema -> "<any>"
       BoolSchema -> "<bool>"
+      NumberSchema -> "<number>"
       StringSchema -> "<string>"
       ArraySchema s -> "-" <+> go s
-      FieldSchema k r s -> pretty k <> (if r then ":" else ":?") <+> go s
-      ApSchema s1 s2 -> align $ vcat [go s1, go s2]
-      CommentSchema t s -> align $ vcat ["# " <> pretty t, go s]
+      FieldSchema k r d s ->
+        let comments =
+              align $ vsep $ map ("# " <>) $
+                catMaybes
+                  [ Just $
+                      if r
+                        then "required"
+                        else "optional",
+                    (("default: " <>) . pretty)
+                      <$> d
+                  ]
+         in vsep [(pretty k <> ":"), indent 2 (go s <+> comments)]
+      ApSchema s1 s2 -> align $ vsep [go s1, go s2]
+      CommentSchema t s -> align $ vsep ["# " <> pretty t, go s]
